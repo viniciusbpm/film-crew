68e2e7083b4c5767a0b0db6f53a9b6bf954bb340
Vinícius <viniciusmpacheco22@gmail.com>
2023-07-23 13:00:02 -0300

    first commit

---
+++ api/src/main/java/com/github/viniciusbpm/filmcrew/FilmCrewApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusbpm.filmcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmCrewApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/github/viniciusbpm/filmcrew/FilmCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusbpm.filmcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmCrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
