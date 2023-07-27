d4c1495f4105bfbbbef67d0370dee214afbe70bd
Vinícius <viniciusmpacheco22@gmail.com>
2023-07-27 07:41:01 -0300

    adding full link to movie images

--- api/src/main/java/com/github/viniciusbpm/filmcrew/service/RequestToTmdbApiService.java
+++ api/src/main/java/com/github/viniciusbpm/filmcrew/service/RequestToTmdbApiService.java
@@ -10,12 +10,12 @@ import static java.lang.System.getenv;
 
 @Service
 public class RequestToTmdbApiService {
-    public static String BASE_API_URL = "https://api.themoviedb.org/3/";
+    public static String BASE_API_URL = "https://api.themoviedb.org/3";
     public static String API_KEY_QUERY_PARAM = "?api_key=" + getenv("API_KEY");
     public MovieListResultDto listPlayingMovies(){
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<MovieListResultDto> resp = restTemplate.getForEntity(format("%smovie/now_playing%s", BASE_API_URL, API_KEY_QUERY_PARAM)
+        ResponseEntity<MovieListResultDto> resp = restTemplate.getForEntity(format("%s/movie/now_playing%s", BASE_API_URL, API_KEY_QUERY_PARAM)
                 , MovieListResultDto.class);
 
         return resp.getBody();
